e88092141aae0e816a5a2afffb3f83cd8ae0b36b
António Moreira <antonioclm0507@gmail.com>
2022-08-01 17:53:19 +0100

    fixes

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/todos', todos)
 app.use(express.static(path.join(__dirname, './frontend/build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build'), (err) => {
+  res.sendFile(path.join(__dirname, './client/build/index.html'), (err) => {
     if (err) {
       res.status(500).send(err)
     }
