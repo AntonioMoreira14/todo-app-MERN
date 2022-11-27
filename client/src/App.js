539bb06ab7230d795e75c278d9fb25568b424458
António Moreira <antonioclm0507@gmail.com>
2022-11-27 12:12:53 +0000

    deploy on render

--- client/src/App.js
+++ client/src/App.js
@@ -1,99 +1,104 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 
 export default function App() {
-  
   const [newInput, setNewInput] = useState("");
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-      fetch('https://todolist-mern-project.herokuapp.com/todos')
-        .then(res => res.json())
-        .then(data => { 
-          let todolist = [];
-          data.todos.forEach(todo => todolist.push(({id: todo._id, msg: todo.msg})))
-          setTodos(todolist);
-        } 
-      )}, [newInput])
+    fetch("https://mern-todo-app-3mhd.onrender.com/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        let todolist = [];
+        data.todos.forEach((todo) =>
+          todolist.push({ id: todo._id, msg: todo.msg })
+        );
+        setTodos(todolist);
+      });
+  }, [newInput]);
 
   function handleChange(e) {
-    setNewInput(e.target.value)
-  } 
+    setNewInput(e.target.value);
+  }
 
   function addTodo() {
-    if(!newInput) {
-      return alert("Enter something on the box to put on the list!")
+    if (!newInput) {
+      return alert("Enter something on the box to put on the list!");
     }
 
     let postOption = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json;charset=utf-8'
+        "Content-Type": "application/json;charset=utf-8",
       },
       body: JSON.stringify({
-				msg: newInput
-			})
-    }
+        msg: newInput,
+      }),
+    };
 
-    let postData = fetch('https://todolist-mern-project.herokuapp.com/todos', postOption)
-    
-    postData
-      .then(res => res.json())
-      setTodos(prevList => [...prevList, postData]);
-      setNewInput("")
+    let postData = fetch(
+      "https://mern-todo-app-3mhd.onrender.com/todos",
+      postOption
+    );
+
+    postData.then((res) => res.json());
+    setTodos((prevList) => [...prevList, postData]);
+    setNewInput("");
   }
 
   function deleteTodo(id) {
-
     let deleteOption = {
-      method: 'DELETE',
+      method: "DELETE",
       headers: {
-        'Content-Type': 'application/json;charset=utf-8'
+        "Content-Type": "application/json;charset=utf-8",
       },
-      body: null
-    }
+      body: null,
+    };
 
-    let deleteData =  fetch(`https://todolist-mern-project.herokuapp.com/todos/${id}`, deleteOption)
-    
-    deleteData.then(res => res.json()).then(() => { 
-        const listTodos = todos.filter(todo => todo.id !== id)
-        setTodos(listTodos)
-    })
+    let deleteData = fetch(
+      `https://mern-todo-app-3mhd.onrender.com/todos/${id}`,
+      deleteOption
+    );
+
+    deleteData
+      .then((res) => res.json())
+      .then(() => {
+        const listTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(listTodos);
+      });
   }
- 
+
   return (
     <div className="App">
       <h1 className="title">My Personal To Do List</h1>
-      <hr className="todo-line"/>
+      <hr className="todo-line" />
       <section className="todo-body">
-      <input 
-        type="text"
-        placeholder="Add a new to do..."
-        value={newInput}
-        onChange={handleChange}
-        className="input-box"
-        maxLength={50}
-      />
-      <button 
-        className="input-button" 
-        onClick={addTodo}>
-        Add To Do
-      </button>
-      <ul>
-        {todos.map(todo => {
-          return (
-            <li key={todo.id}>{todo.msg}
-              <button 
-                className="delete-btn" 
-                onClick={() => deleteTodo(todo.id)}
-              >
-                <i className="fa-regular fa-trash-can"></i>
-              </button>
-            </li>
-           )
-        })}
+        <input
+          type="text"
+          placeholder="Add a new to do..."
+          value={newInput}
+          onChange={handleChange}
+          className="input-box"
+          maxLength={50}
+        />
+        <button className="input-button" onClick={addTodo}>
+          Add To Do
+        </button>
+        <ul>
+          {todos.map((todo) => {
+            return (
+              <li key={todo.id}>
+                {todo.msg}
+                <button
+                  className="delete-btn"
+                  onClick={() => deleteTodo(todo.id)}
+                >
+                  <i className="fa-regular fa-trash-can"></i>
+                </button>
+              </li>
+            );
+          })}
         </ul>
       </section>
     </div>
-  )
+  );
 }
-
--- index.js
+++ index.js
@@ -1,43 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const todos = require('./routes/todos')
-const connectDB = require('./db/connect')
-require('dotenv').config()
-const path = require('path')
-const cors = require('cors')
+const todos = require("./routes/todos");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const path = require("path");
+const cors = require("cors");
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.use(cors())
+app.use(cors());
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 
 //routes
 
-app.use('/todos', todos)
+app.use("/todos", todos);
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './client/build', 'index.html'))
-})
-
-
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
-    app.listen(port, console.log(`server is listening on port ${port}....`))
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}....`));
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
 
 start();
